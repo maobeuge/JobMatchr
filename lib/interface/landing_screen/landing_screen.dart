307b5d90a07e02d247a523f2f26e936029cd8c62
Romain <romainflorianipro@gmail.com>
2024-08-21 16:40:17 +0200

    feat(PageView): Animation when navigating through different screens

--- lib/interface/landing_screen/landing_screen.dart
+++ lib/interface/landing_screen/landing_screen.dart
@@ -2,29 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jobmatchr/interface/landing_screen/nav_bar.dart';
 import 'package:jobmatchr/interface/landing_screen/top_bar.dart';
-import 'package:jobmatchr/interface/profile_screen/profile_search.dart';
+import 'package:jobmatchr/interface/message_screen/message_screen.dart';
+import 'package:jobmatchr/interface/profile_screen/profile_screen.dart';
+import 'package:jobmatchr/interface/swipe_screen/profile_search.dart';
 import 'package:jobmatchr/services/navigation/navigation_bloc.dart';
+
+const List<Widget> screenList = [
+  MessageScreen(),
+  ProfileSwipeScreen(),
+  ProfileScreen()
+];
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final PageController pageViewController =
+        PageController(initialPage: NavigationIndex.swipe.index);
+
     return BlocProvider(
-      create: (context) => NavigationBloc(),
+      create: (context) =>
+          NavigationBloc(pageViewController: pageViewController),
       child: Scaffold(
         backgroundColor: Colors.black,
         body: BlocBuilder<NavigationBloc, NavigationState>(
           builder: (context, state) {
             if (state is NavigationLoaded) {
-              return const SafeArea(
+              return SafeArea(
                 child: Stack(
                   children: [
-                    TopBar(),
-                    AppNavBar(),
+                    const TopBar(),
+                    const AppNavBar(),
                     Padding(
-                      padding: EdgeInsets.only(bottom: 70.0, top: 60.0),
-                      child: ProfileSwipeScreen(),
+                      padding: const EdgeInsets.only(bottom: 70.0, top: 60.0),
+                      child: PageView.builder(
+                        controller: pageViewController,
+                        itemCount: NavigationIndex.values.length,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return screenList[index];
+                        },
+                      ),
                     ),
                   ],
                 ),
--- lib/services/navigation/navigation_bloc.dart
+++ lib/services/navigation/navigation_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -7,13 +8,20 @@ part 'navigation_state.dart';
 enum NavigationIndex { message, swipe, profile }
 
 class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
-  NavigationBloc()
+  final PageController pageViewController;
+
+  NavigationBloc({required this.pageViewController})
       : super(const NavigationLoaded(index: NavigationIndex.swipe)) {
     on<LoadNavigation>((event, emit) {
       emit(const NavigationLoaded(index: NavigationIndex.swipe));
     });
 
     on<ChangeIndex>((event, emit) {
+      pageViewController.animateToPage(
+        event.navigationIndex.index,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+      );
       emit(NavigationLoaded(index: event.navigationIndex));
     });
   }
